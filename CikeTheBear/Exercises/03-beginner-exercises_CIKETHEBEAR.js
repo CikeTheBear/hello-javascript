cefe61841833b2f220e75e235f114a61c16dfbed
CikeTheBear <dealmeidaromanelli@gmail.com>
2025-06-14 12:06:31 -0400

    Update

---
+++ CikeTheBear/Exercises/03-beginner-exercises_CIKETHEBEAR.js
@@ -0,0 +1,57 @@
+/*
+Clase 18 - Ejercicios: primeros pasos
+Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
+*/
+
+// 1. Escribe un comentario en una línea
+
+// Este es un comentario de una linea, se crea utilizando dos barras inclinadas al inicio de la linea. 
+
+// 2. Escribe un comentario en varias líneas
+
+/* Este es un comentario de varias lineas, se crea usando una barra inclinada seguida de un asterisco al inicio de la linea
+y se finaliza con un asterisco seguido de una barra inclinada al final de la linea.*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+let boolValue = true
+let nullValue = null
+let undefinedValue = undefined
+let integerValue = 33
+let floatValue = 3.14
+let bigIntValue = BigInt(123456789123456789)
+let stringValue = "Hola, me llamo Carlos"
+let symbolValue = Symbol("Soy un simbolo, y aun no se para que sirve xD")
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(boolValue)
+console.log(nullValue)
+console.log(undefinedValue)
+console.log(integerValue)
+console.log(floatValue)
+console.log(bigIntValue)
+console.log(stringValue)
+console.log(symbolValue)
+
+// 5. Imprime por consola el tipo de todas las variables
+
+console.log(typeof boolValue)
+console.log(typeof nullValue)
+console.log(typeof undefinedValue)
+console.log(typeof integerValue)
+console.log(typeof floatValue)
+console.log(typeof bigIntValue)
+console.log(typeof stringValue)
+console.log(typeof symbolValue)
+
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuación, modifica los valores de las constantes
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
